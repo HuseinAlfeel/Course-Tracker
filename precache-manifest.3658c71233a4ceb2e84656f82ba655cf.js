935b58c3993efc45268de33ebab4ce48f2566126
HuseinAlfeel <husin.alfil@student.hpi.uni-potsdam.de>
2025-05-15 11:57:39 +0200

    Updates

---
+++ precache-manifest.3658c71233a4ceb2e84656f82ba655cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37743a80c1af538edecc261485f0774f",
+    "url": "/Course-Tracker/index.html"
+  },
+  {
+    "revision": "c3008eb1cbfbd9fb9952",
+    "url": "/Course-Tracker/static/css/main.60d33e81.chunk.css"
+  },
+  {
+    "revision": "dbf97cece0c01fa65d65",
+    "url": "/Course-Tracker/static/js/2.02fa92f4.chunk.js"
+  },
+  {
+    "revision": "c3008eb1cbfbd9fb9952",
+    "url": "/Course-Tracker/static/js/main.7c0ffa38.chunk.js"
+  },
+  {
+    "revision": "9e598a7336762b38ada8",
+    "url": "/Course-Tracker/static/js/runtime~main.14e160f6.js"
+  }
+]);
